7bb1a2e62219f957827ec63f5eeb0dece193873a
s29826 <s29826@pjwstk.edu.pl>
2025-03-04 21:33:44 +0100

    Modyfikacja

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -1,2 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿static double Avg(int[] arr)
+{
+    int suma = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        suma += arr[i];
+    }
+    return (float) suma / arr.Length;
+}
+int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+Console.WriteLine(Avg(arr));
