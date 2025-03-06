ba19d7fc76502db7bfcb39222e2add8f8fa4ebdc
s29826 <s29826@pjwstk.edu.pl>
2025-03-06 22:08:55 +0100

    feat: average

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World and User!");
+﻿static double Average(int[] arr)
+{
+    int suma = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        suma += arr[i];
+    }
+
+    return suma / arr.Length;
+}
+int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+Console.WriteLine(Average(arr));
